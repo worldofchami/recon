aecf3c2688eccb4a21ee1b909b072dbb579ab19d
TINO_CHAMI <TCHAMINUKA19@GMAIL.COM>
2026-01-19 17:15:00 +0200

    Hardcoding API url to debug

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
-  //   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
-  // },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NODE_ENV === "production" ? "https://recon-638214892937.europe-west3.run.app" : 'http://localhost:8000',
+  },
 }
 
 module.exports = nextConfig
